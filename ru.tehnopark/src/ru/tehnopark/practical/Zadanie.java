04f8d242d56a891246a2c2a63e487c807dc910fa
AleshaGlebov2000 <95049987+AleshaGlebov2000@users.noreply.github.com>
2022-04-14 12:41:23 +0300

    Add files via upload

---
+++ ru.tehnopark/src/ru/tehnopark/practical/Zadanie.java
@@ -0,0 +1,26 @@
+package Lesson_3;
+
+public class Zadanie {
+    public static void main(String[] args)
+            throws java.io.IOException {
+        char letter = 'q';
+        char probel = ' ';
+        char input;
+        int count = 0;
+        System.out.println("Введите букву");
+       do {
+           input = (char) System.in.read();
+           if (input == letter ) {
+               System.out.println("Вы угадали");
+           } else if (input == probel) {
+              count++;
+               System.out.println("Пробел");
+           }
+           else {
+               System.out.println("Попробуйте еще раз");
+           }
+       }
+           while (letter != input) ;
+           System.out.println("Счетчик пробелов: " + count);
+        }
+    }
